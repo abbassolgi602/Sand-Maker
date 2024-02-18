317983679f702c9c556966b38839a230701f1391
abbassolgi602 <abbassolgi602@gmail.com>
2024-02-18 14:35:50 +0330

    created files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Circles Attak (Demo)
+
+const canvas = document.querySelector('#canvas');
+const points = document.querySelector('#points');
+const pointsNumber = points.querySelector('span');
+
+const ctx = canvas.getContext('2d');
